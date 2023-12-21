0b22ae0f09a8dbe5eac6a137099a0a3559e87a70
RylaD303 <alemak2002@gmail.com>
2023-12-21 23:18:24 +0200

    Added test node.

---
+++ gdextension/src/register_types.cpp
@@ -0,0 +1,58 @@
+/* godot-cpp integration testing project.
+ *
+ * This is free and unencumbered software released into the public domain.
+ */
+
+#include "register_types.h"
+
+#include <gdextension_interface.h>
+
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+#include "test.hpp"
+
+#include "components/area2d/hitbox.hpp"
+#include "components/area2d/hurtbox.hpp"
+#include "components/health/health_component.hpp"
+#include "projectiles/projectile.hpp"
+
+
+using namespace godot;
+
+void initialize_gdextension_module(ModuleInitializationLevel p_level)
+{
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+	{
+		return;
+	}
+	ClassDB::register_class<Test>();
+	ClassDB::register_class<Projectile>();
+	ClassDB::register_class<HealthComponent>();
+	ClassDB::register_class<Hitbox>();
+	ClassDB::register_class<Hurtbox>();
+}
+
+void uninitialize_gdextension_module(ModuleInitializationLevel p_level)
+{
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+	{
+		return;
+	}
+}
+
+extern "C"
+{
+// Initialization.
+GDExtensionBool GDE_EXPORT gdextension_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
+{
+	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+
+	init_obj.register_initializer(initialize_gdextension_module);
+	init_obj.register_terminator(uninitialize_gdextension_module);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+	return init_obj.init();
+}
+}
---
+++ gdextension/src/test.hpp
@@ -0,0 +1,17 @@
+#ifndef TEST1_HPP
+#define TEST1_HPP
+
+#include "godot_cpp/classes/node2d.hpp"
+
+using namespace godot;
+
+class Test : public Node2D
+{
+    GDCLASS(Test, Node2D);
+
+    protected:
+    static void _bind_methods(){};
+
+};
+
+#endif // TEST1_HPP
