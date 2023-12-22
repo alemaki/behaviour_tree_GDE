4dd07a0e7b54547ff9f24b0835d3344edca6fd04
RylaD303 <alemak2002@gmail.com>
2023-12-22 18:38:12 +0200

    Added health component tests.

---
+++ gdextension/src/components/health/health_component.cpp
@@ -0,0 +1,46 @@
+#include "health_component.hpp"
+#include <godot_cpp/core/math.hpp>
+
+
+void HealthComponent::_bind_methods()
+{
+    godot::ClassDB::bind_method(godot::D_METHOD("set_max_hp", "max_hp"), &HealthComponent::set_max_hp);
+    godot::ClassDB::bind_method(godot::D_METHOD("get_max_hp"), &HealthComponent::get_max_hp);
+    ADD_PROPERTY(godot::PropertyInfo(godot::Variant::INT, "max_hp"), "set_max_hp", "get_max_hp");
+
+    godot::ClassDB::bind_method(godot::D_METHOD("set_current_hp", "current_hp"), &HealthComponent::set_current_hp);
+    godot::ClassDB::bind_method(godot::D_METHOD("get_current_hp"), &HealthComponent::get_current_hp);
+    ADD_PROPERTY(godot::PropertyInfo(godot::Variant::INT, "current_hp"), "set_current_hp", "get_current_hp");    
+}
+
+void HealthComponent::set_max_hp(int max_hp)
+{
+    max_hp = godot::Math::clamp<int>(max_hp, 0, max_hp);
+    this->max_hp = max_hp;
+}
+
+int HealthComponent::get_max_hp() const
+{
+    return this->max_hp;
+}
+
+void HealthComponent::set_current_hp(int current_hp)
+{
+    current_hp = godot::Math::clamp<int>(current_hp, 0, this->max_hp);
+    this->current_hp = current_hp;
+}
+
+int HealthComponent::get_current_hp() const
+{
+    return this->current_hp;
+}
+
+void HealthComponent::take_damage(int amount)
+{
+    amount = godot::Math::clamp<int>(amount, 0, amount);
+    this->current_hp -= amount;
+    if (current_hp < 0)
+    {
+        current_hp = 0;
+    }
+}
---
+++ gdextension/src/components/health/health_component.hpp
@@ -0,0 +1,27 @@
+#ifndef HEALTH_COMPONENT_HPP
+#define HEALTH_COMPONENT_HPP
+
+
+#include <godot_cpp/classes/Node.hpp>
+
+class HealthComponent : public godot::Node
+{
+    GDCLASS(HealthComponent, godot::Node);
+    
+public:
+    int max_hp;
+    int current_hp;
+
+public:
+    void set_max_hp(int max_hp);
+    int get_max_hp() const;
+    void set_current_hp(int current_hp);
+    int get_current_hp() const;
+    void take_damage(int amount);
+    
+
+protected:
+    static void _bind_methods();
+};
+
+#endif
