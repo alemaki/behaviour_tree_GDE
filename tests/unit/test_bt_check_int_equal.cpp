4941a9696baa96af17fce72001a20202c965438a
Alexander Makedonski <alemak2002@gmail.com>
2024-06-06 16:43:37 +0300

    Added tests for condition check int equal.

---
+++ tests/unit/test_bt_check_int_equal.cpp
@@ -0,0 +1,47 @@
+#define DOCTEST_CONFIG_NO_EXCEPTIONS_BUT_WITH_ALL_ASSERTS
+#include <doctest.h>
+#include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+
+#include "tests/test_utils/test_runner.hpp"
+#include "conditions/bt_check_int_equal.hpp"
+#include "blackboard/blackboard.hpp"
+
+TEST_SUITE("BTCheckIntEqual")
+{
+    TEST_CASE("Basics")
+    {
+        godot::Ref<BTCheckIntEqual> task = memnew(BTCheckIntEqual);
+        task->set_value(10);
+        task->set_var_name("TestVar");
+
+        godot::Node* actor = memnew(godot::Node);
+        
+        godot::Ref<Blackboard> blackboard = memnew(Blackboard);
+        blackboard->set_var("TestVar", 10);
+
+        task->initialize(actor, blackboard);
+
+        SUBCASE("Value matches")
+        {
+            BTTask::Status status = task->execute(0.1);
+            CHECK(status == BTTask::Status::SUCCESS);
+        }
+
+        SUBCASE("Value does not match")
+        {
+            blackboard->set_var("TestVar", 5);
+            BTTask::Status status = task->execute(0.1);
+            CHECK(status == BTTask::Status::FAILURE);
+        }
+
+        SUBCASE("Variable does not exist")
+        {
+            blackboard->erase_var("TestVar");
+            BTTask::Status status = task->execute(0.1);
+            CHECK(status == BTTask::Status::FAILURE);
+        }
+
+        memdelete(actor);
+    }
+}
