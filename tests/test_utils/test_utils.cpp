8578415297d53cf4703defe60e66282a35eb271c
Alexander Makedonski <alemak2002@gmail.com>
2024-07-20 11:15:54 +0300

    Updated test utils. Fixed bug with process running before physics on some nodes.

--- tests/test_utils/test_utils.cpp
+++ tests/test_utils/test_utils.cpp
@@ -1,20 +1,27 @@
 #include "test_utils.hpp"
 
 #include <godot_cpp/classes/engine.hpp>
-#include <godot_cpp/variant/utility_functions.hpp>
 
-#include "tests/test_utils/test_runner.hpp"
-
-void simulate_frame(godot::Node* node, float delta)
+void simulate_frame_physics_process(godot::Node* node, float delta)
 {
 	godot::Array children = node->get_children();
 	int size = children.size();
 	for (int i = 0; i < size; i++)
 	{
-		simulate_frame(godot::Object::cast_to<godot::Node>(children[i]), delta);
+		simulate_frame_physics_process(godot::Object::cast_to<godot::Node>(children[i]), delta);
+	}
+	node->_physics_process(delta);
+}
+
+void simulate_frame_process(godot::Node* node, float delta)
+{
+	godot::Array children = node->get_children();
+	int size = children.size();
+	for (int i = 0; i < size; i++)
+	{
+		simulate_frame_process(godot::Object::cast_to<godot::Node>(children[i]), delta);
 	}
 	node->_process(delta);
-	node->_physics_process(delta);
 
 }
 
@@ -28,7 +35,11 @@ void simulate(godot::Node* node, int frames)
 	ERR_FAIL_COND(delta == 0);
 	for (int i = 0; i < frames; ++i)
     {
-		simulate_frame(node, delta);
+		simulate_frame_process(node, delta);
+	}
+	for (int i = 0; i < frames; ++i)
+    {
+		simulate_frame_physics_process(node, delta);
 	}
 }
 
@@ -42,13 +53,27 @@ double get_node_delta(godot::Node* node)
 	return  godot::Engine::get_singleton()->is_in_physics_frame() ? node->get_physics_process_delta_time() : node->get_process_delta_time();
 }
 
-bool vectors_almost_equal(godot::Vector2 vec1, godot::Vector2 vec2, bool complain)
+bool vectors_almost_eq(godot::Vector2 vec1, godot::Vector2 vec2, bool complain)
 {
 	bool result = doctest::Approx(vec1.x) == vec2.x && doctest::Approx(vec1.y) == vec2.y;
 
 	if ((!result) && (complain))
 	{
-		godot::UtilityFunctions::print(vec1, " != ", vec2);
+		godot::UtilityFunctions::printerr("Test error.");
+		godot::UtilityFunctions::printerr(vec1, " != ", vec2);
+	}
+    return result;
+}
+
+
+bool vectors_almost_ne(godot::Vector2 vec1, godot::Vector2 vec2, bool complain)
+{
+	bool result = doctest::Approx(vec1.x) != vec2.x || doctest::Approx(vec1.y) != vec2.y;
+
+	if ((!result) && (complain))
+	{
+		godot::UtilityFunctions::printerr("Test error.");
+		godot::UtilityFunctions::printerr(vec1, " == ", vec2);
 	}
     return result;
 }
--- tests/test_utils/test_utils.hpp
+++ tests/test_utils/test_utils.hpp
@@ -2,13 +2,38 @@
 #define BT_TEST_UTILS_HPP
 
 #include <doctest.h>
-#include<godot_cpp/classes/node.hpp>
+#include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
 
-void simulate(godot::Node* node,int frames = 1);
+#include "tests/test_utils/test_runner.hpp"
+
+void simulate(godot::Node* node, int frames = 1);
 
 double get_node_delta(godot::Node* node);
 double get_current_engine_delta();
 
-bool vectors_almost_equal(godot::Vector2 vec1, godot::Vector2 vec2, bool complain = true);
+bool vectors_almost_eq(godot::Vector2 vec1, godot::Vector2 vec2, bool complain = true);
+bool vectors_almost_ne(godot::Vector2 vec1, godot::Vector2 vec2, bool complain = true);
+
+#ifdef CHECK_VECTORS_EQ
+#undef CHECK_VECTORS_EQ
+#endif
+#define CHECK_VECTORS_EQ(vec1, vec2) CHECK(::vectors_almost_eq(vec1, vec2, true));
+
+#ifdef CHECK_VECTORS_NE
+#undef CHECK_VECTORS_NE
+#endif
+#define CHECK_VECTORS_NE(vec1, vec2) CHECK(::vectors_almost_ne(vec1, vec2, true));
+
+
+#ifdef REQUIRE_VECTORS_EQ
+#undef REQUIRE_VECTORS_EQ
+#endif
+#define REQUIRE_VECTORS_EQ(vec1, vec2) REQUIRE(::vectors_almost_eq(vec1, vec2, true));
+
+#ifdef REQUIRE_VECTORS_NE
+#undef REQUIRE_VECTORS_NE
+#endif
+#define REQUIRE_VECTORS_NE(vec1, vec2) REQUIRE(::vectors_almost_ne(vec1, vec2, true));
 
 #endif /* BT_TEST_UTILS_HPP */
