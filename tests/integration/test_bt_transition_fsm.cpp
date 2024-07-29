2a67d70e5cfbbc36a2e61ff013f586ad7f901aa5
Alexander Makedonski <alemak2002@gmail.com>
2024-07-29 16:31:47 +0300

    Added tests for BTTransitionFSM.

---
+++ tests/integration/test_bt_transition_fsm.cpp
@@ -0,0 +1,67 @@
+#define DOCTEST_CONFIG_NO_EXCEPTIONS_BUT_WITH_ALL_ASSERTS
+#include <doctest.h>
+#include <godot_cpp/classes/node.hpp>
+
+#include "tests/test_utils/test_runner.hpp"
+#include "behaviour/actions/bt_transition_fsm.hpp"
+#include "blackboard/blackboard.hpp"
+#include "finite_state_machine/fsm.hpp"
+
+struct TransitionFSMFixture
+{
+    godot::Ref<BTTransitionFSM> task;
+    godot::Node* actor;
+    godot::Ref<Blackboard> blackboard;
+    FSM* fsm;
+
+    TransitionFSMFixture() : task(memnew(BTTransitionFSM)),
+                             actor(memnew(godot::Node)),
+                             blackboard(memnew(Blackboard)),
+                             fsm(memnew(FSM))
+    {
+        task->set_complain(false);
+        task->set_fsm_name("TestFSM");
+        task->set_to_state("running");
+
+        fsm->set_initial_state("idle");
+        fsm->add_transition("idle", "running");
+
+        blackboard->set_var("TestFSM", fsm);
+
+        task->initialize(actor, blackboard);
+    }
+
+    ~TransitionFSMFixture()
+    {
+        memdelete(actor);
+        memdelete(fsm);
+    };
+};
+
+TEST_SUITE("BTTransitionFSM")
+{
+    TEST_CASE_FIXTURE(TransitionFSMFixture, "FSM can transition to target state")
+    {
+        BTTask::Status status = task->execute(0.1);
+        CHECK_EQ(status, BTTask::Status::SUCCESS);
+        CHECK_EQ(fsm->get_state(), "running");
+        godot::UtilityFunctions::print(fsm->get_state());
+        godot::UtilityFunctions::print(task->get_to_state());
+    }
+
+    TEST_CASE_FIXTURE(TransitionFSMFixture, "FSM cannot transition to target state")
+    {
+        fsm->add_transition("running", "stopped");
+        task->set_to_state("stopped");
+
+        BTTask::Status status = task->execute(0.1);
+        CHECK_EQ(status, BTTask::Status::FAILURE);
+    }
+
+    TEST_CASE_FIXTURE(TransitionFSMFixture, "FSM does not exist in Blackboard")
+    {
+        blackboard->erase_var("TestFSM");
+        BTTask::Status status = task->execute(0.1);
+        CHECK_EQ(status, BTTask::Status::FAILURE);
+    }
+};
