05c67cdee3f8d4698eaafadf1bf3e84a4a6fbb2f
Alexander Makedonski <alemak2002@gmail.com>
2024-08-12 09:55:27 +0300

    Added classdb binding macros.

--- behaviour_tree/utils/macros.hpp
+++ behaviour_tree/utils/macros.hpp
@@ -3,6 +3,7 @@
 
 #include <godot_cpp/variant/utility_functions.hpp>
 #include <godot_cpp/core/math.hpp>
+#include <godot_cpp/core/class_db.hpp>
 
 #define CREATE_GETTER_SETTER(type, name, member) /*****************************************************************************************************************************/\
     void set_##name(type value) { this->member = value; }                                                                                                                       \
@@ -44,6 +45,28 @@
     CREATE_GETTER_SETTER_POSITIVE(type, member, member)
 
 
+#define BIND_GETTER_SETTER(class, name, parameter) /***************************************************************************************************************************/\
+    ClassDB::bind_method(D_METHOD("set_" #name, #parameter), &class::set_##name);                                                                                               \
+    ClassDB::bind_method(D_METHOD("get_" #name), &class::get_##name);
+
+#define BIND_GETTER_SETTER_DEFAULT(class, name) /******************************************************************************************************************************/\
+    ClassDB::bind_method(D_METHOD("set_" #name, #name), &class::set_##name);                                                                                                    \
+    ClassDB::bind_method(D_METHOD("get_" #name), &class::get_##name);
+
+#define BIND_PROPERTY(name, variant_type, member) /****************************************************************************************************************************/\
+    ADD_PROPERTY(PropertyInfo(Variant::variant_type, #member), "set_" #name, "get_" #name);
+
+#define BIND_PROPERTY_DEFAULT(variant_type, member) /**************************************************************************************************************************/\
+    ADD_PROPERTY(PropertyInfo(Variant::variant_type, #member), "set_" #member, "get_" #member);
+
+#define BIND_GETTER_SETTER_PROPERTY(class, name, parameter, variant_type, member) /********************************************************************************************/\
+    BIND_GETTER_SETTER(class, name, parameter)                                                                                                                                  \
+    BIND_PROPERTY(name, variant_type, member)
+
+#define BIND_GETTER_SETTER_PROPERTY_DEFAULT(class, variant_type, member) /*****************************************************************************************************/\
+    BIND_GETTER_SETTER_DEFAULT(class, member)                                                                                                                                   \
+    BIND_PROPERTY_DEFAULT(variant_type, member)
+
 // include bt_task after so it can use definitions for getter_setter
 #include "behaviour_tree/tasks/bt_task.hpp"
 
@@ -54,7 +77,7 @@
     return BTTask::Status::SUCCESS
 
 #define TASK_COMPLAIN_COND(condition, message) /*******************************************************************************************************************************/\
-    if ((condition) && (this->is_complain_enabled()))                                                                                                                                        \
+    if ((condition) && (this->is_complain_enabled()))                                                                                                                           \
     {                                                                                                                                                                           \
         godot::UtilityFunctions::printerr(message);                                                                                                                             \
     }                                                                                                                                                                           \
