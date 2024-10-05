c7473b147074eec218a324f64fa3b88f73a150cc
Alexander Makedonski <alemak2002@gmail.com>
2024-10-05 23:42:48 +0300

    Added tests for BTGraphView.

--- behaviour_tree/ui/bt_graph_view.cpp
+++ behaviour_tree/ui/bt_graph_view.cpp
@@ -9,7 +9,7 @@ void BTGraphView::create_task_node(const godot::StringName& task_name, const god
 {
     ERR_FAIL_COND(task_name.is_empty());
     ERR_FAIL_COND(task_class_name.is_empty());
-    ERR_FAIL_COND(!(this->has_task_name(task_name)));
+    ERR_FAIL_COND_MSG(this->has_task_name(task_name), "BTGraphView already has node named: " + task_name + ".");
 
     BTGraphNode* graph_node = memnew(BTGraphNode);
     this->task_name_to_node[task_name] = graph_node;
@@ -19,7 +19,7 @@ void BTGraphView::create_task_node(const godot::StringName& task_name, const god
 
 void BTGraphView::set_task_title(const godot::StringName& task_name, const godot::String& task_title)
 {
-    ERR_FAIL_COND(!(this->has_task_name(task_name)));
+    ERR_FAIL_COND_MSG(!(this->has_task_name(task_name)), "BTGraphView has no node named: " + task_name + ".");
 
     BTGraphNode* graph_node = this->task_name_to_node[task_name];
     graph_node->set_title(task_title);
@@ -27,7 +27,7 @@ void BTGraphView::set_task_title(const godot::StringName& task_name, const godot
 
 void BTGraphView::delete_task_node(const godot::StringName& task_name)
 {
-    ERR_FAIL_COND(!(this->has_task_name(task_name)));
+    ERR_FAIL_COND_MSG(!(this->has_task_name(task_name)), "BTGraphView has no node named: " + task_name + ".");
 
     BTGraphNode* graph_node = this->task_name_to_node[task_name];
     this->remove_child(graph_node);
@@ -37,7 +37,7 @@ void BTGraphView::delete_task_node(const godot::StringName& task_name)
 
 void BTGraphView::change_task_class_name(const godot::StringName& task_name, const godot::StringName& task_class_name)
 {
-    ERR_FAIL_COND(!(this->has_task_name(task_name)));
+    ERR_FAIL_COND_MSG(!(this->has_task_name(task_name)), "BTGraphView has no node named: " + task_name + ".");
 
     BTGraphNode* graph_node = this->task_name_to_node[task_name];
     graph_node->set_task_class_name(task_class_name);
--- tests/unit/editor/test_bt_graph_view.cpp
+++ tests/unit/editor/test_bt_graph_view.cpp
@@ -9,7 +9,17 @@
 struct BTGraphViewFixture
 {
     BTGraphView* graph_view = memnew(BTGraphView);
-    BTGraphViewFixture(){}
+    int initial_child_count = 0;
+    BTGraphViewFixture()
+    {
+        this->initial_child_count = graph_view->get_child_count();
+    }
+
+    BTGraphNode* get_graph_node(int index) const
+    {
+        return godot::Object::cast_to<BTGraphNode>(graph_view->get_child(initial_child_count + index));
+    }
+
     ~BTGraphViewFixture()
     {
         memdelete(graph_view);
@@ -25,9 +35,58 @@ TEST_SUITE("[editor]" "BTGraphView")
         REQUIRE_FALSE(graph_view->is_undo_redo_enabled());
     }
 
-    TEST_CASE_FIXTURE(BTGraphViewFixture, "Setters and getters")
+    TEST_CASE_FIXTURE(BTGraphViewFixture, "Create task node")
     {
-        REQUIRE_EQ(graph_view->get_undo_redo_manager(), nullptr);
-        REQUIRE_FALSE(graph_view->is_undo_redo_enabled());
+        graph_view->create_task_node("task_1", "TaskClass");
+
+        REQUIRE(graph_view->has_task_name("task_1"));
+    }
+
+    TEST_CASE_FIXTURE(BTGraphViewFixture, "Set task title")
+    {
+        graph_view->create_task_node("task_1", "TaskClass");
+
+        graph_view->set_task_title("task_1", "Task Title");
+        BTGraphNode* graph_node = this->get_graph_node(0);
+        REQUIRE_EQ(graph_node->get_title(), godot::StringName("Task Title"));
+    }
+
+    TEST_CASE_FIXTURE(BTGraphViewFixture, "Delete task node")
+    {
+        graph_view->create_task_node("task_1", "TaskClass");
+
+        graph_view->delete_task_node("task_1");
+
+        REQUIRE_FALSE(graph_view->has_task_name("task_1"));
+    }
+
+    TEST_CASE_FIXTURE(BTGraphViewFixture, "Change task class name")
+    {
+        graph_view->create_task_node("task_1", "InitialClass");
+
+        graph_view->change_task_class_name("task_1", "NewClass");
+        BTGraphNode* graph_node = this->get_graph_node(0);
+        REQUIRE_EQ(graph_node->get_task_class_name(), godot::StringName("NewClass"));
+    }
+
+    TEST_CASE_FIXTURE(BTGraphViewFixture, "Fail to create task with empty names")
+    {
+        CHECK_THROWS(graph_view->create_task_node("", "TaskClass"));
+        CHECK_THROWS(graph_view->create_task_node("task_1", ""));
+    }
+
+    TEST_CASE_FIXTURE(BTGraphViewFixture, "Fail to set title of non-existent task")
+    {
+        CHECK_THROWS(graph_view->set_task_title("non_existent", "Title"));
+    }
+
+    TEST_CASE_FIXTURE(BTGraphViewFixture, "Fail to delete non-existent task node")
+    {
+        CHECK_THROWS(graph_view->delete_task_node("non_existent"));
+    }
+
+    TEST_CASE_FIXTURE(BTGraphViewFixture, "Fail to change class name of non-existent task")
+    {
+        CHECK_THROWS(graph_view->change_task_class_name("non_existent", "NewClass"));
     }
 }
