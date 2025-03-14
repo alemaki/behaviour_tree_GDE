964b2d00afed50975bc3cadb1bf32555a7b1a340
Alexander Makedonski <alemak2002@gmail.com>
2025-03-14 17:26:59 +0200

    Added documentation for Sequence.

--- behaviour_tree/tasks/composites/bt_sequence.hpp
+++ behaviour_tree/tasks/composites/bt_sequence.hpp
@@ -3,6 +3,22 @@
 
 #include "behaviour_tree/tasks/bt_composite.hpp"
 
+/**
+ * @class BTSequence
+ * @brief Behaviour Tree composite that executes its children in order until one fails.
+ *
+ * The Sequence node ensures all its children succeed in sequence before succeeding itself.
+ * If any child fails, the entire sequence fails immediately.
+ * If a child is still running, the sequence remains running and resumes from that child next tick.
+ *
+ * Return Conditions:
+ *
+ * - SUCCESS: All children return SUCCESS.
+ *
+ * - RUNNING: A child returns RUNNING.
+ *
+ * - FAILURE: A child returns FAILURE.
+ */
 class BTSequence : public BTComposite
 {
 	GDCLASS(BTSequence, BTComposite);
