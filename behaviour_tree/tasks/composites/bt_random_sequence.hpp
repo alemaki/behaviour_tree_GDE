6e20b0a2f3b5e459ab6dacf8a3bfdfd30138ac5f
Alexander Makedonski <alemak2002@gmail.com>
2025-03-15 17:28:55 +0200

    Added documentation for RandomSequence.

--- behaviour_tree/tasks/composites/bt_random_sequence.hpp
+++ behaviour_tree/tasks/composites/bt_random_sequence.hpp
@@ -3,6 +3,24 @@
 
 #include "behaviour_tree/tasks/bt_composite.hpp"
 
+/**
+ * @class BTRandomSequence
+ * @brief Behaviour Tree composite that executes all children in a random order.
+ *
+ * The Random Sequence shuffles its children each time execution begins
+ * and evaluates them one by one.
+ * The sequence only succeeds if all children succeed.
+ * If any child fails, the sequence fails immediately.
+ * If a child is running, the sequence remains running and resumes from that child next tick.
+ *
+ * Return Conditions:
+ *
+ * - SUCCESS: All children return SUCCESS.
+ *
+ * - RUNNING: A child returns RUNNING.
+ *
+ * - FAILURE: A child returns FAILURE.
+ */
 class BTRandomSequence : public BTComposite
 {
 	GDCLASS(BTRandomSequence, BTComposite);
